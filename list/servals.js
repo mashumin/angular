22fbdb50775f9829d6b032e6c3baa25f65c057ea
msm <1345241789@qq.com>
2017-01-06 09:02:49 +0800

    m

--- list/servals.js
+++ list/servals.js
@@ -1,9 +1,7 @@
 angular.module("sevices",[])
     .factory("indexDate",function($http){
-        // return [{title:"标题1",con:"内容"}]
         return $http({url:"1.txt"})
     })
     .factory("listDate",function($http){
-        // return [{title:"标题1",con:"内容"}]
         return $http({url:"1.txt"})
     })
